89af7e33fe672043639c7099aa8d736e7c45e44f
Ivanhai <shatlov.ivan.evgenyevicc@gmail.com>
2021-10-20 09:11:24 +0300

    working

--- src/jvmMain/kotlin/app.kt
+++ src/jvmMain/kotlin/app.kt
@@ -1,10 +1,11 @@
 import java.util.*
 import kotlin.math.sqrt
+import kotlin.random.Random
 
 fun main() {
-    show(generateFull(listOf(0,1,2,3)))
+    show(generateWithZeros(listOf(0,1,2,3,4,5)))
 }
-fun generateFull(seed : List<Int>): List<List<Int>> {
+fun generateFull(seed : List<Int>): MutableList<List<Int>> {
     val box : MutableList<List<Int>> = MutableList(1) {
         MutableList(seed.size) {
             seed[it]
@@ -12,11 +13,32 @@ fun generateFull(seed : List<Int>): List<List<Int>> {
     }
     val size = sqrt(seed.size.toDouble()).toInt()
 
-    box.add(box[0].shift(size) )
-    box.add(box[1].subshift(size) )
-    box.add(box[0].subshift(size) )
-    println(box)
+    for(i in 0 until size - 1) {
+        box.add(box[i].shift(size))
+    }
+    if(size > 2) {
+        for(i in 0 until size - 1) {
+            box.add(box[i].subshift(size))
+        }
+    } else {
+        for(i in 0 until size) {
+            box.add(box[i].subshift(size))
+        }
+    }
+    if(size > 2) {
+        for (i in 2 until size * 2) {
+            box.add(box[i].subshift(size))
+        }
+    }
     return box
+}
+
+fun generateWithZeros(seed : List<Int>): MutableList<MutableList<Int>> {
+    val list = generateFull(seed) as MutableList<MutableList<Int>>
+    for(i in 0..4) {
+        list[Random.nextInt(seed.size - 1)][Random.nextInt(seed.size - 1)] = 0
+    }
+    return list
 }
 
 fun show(list : List<List<Int>>) {
@@ -26,7 +48,7 @@ fun show(list : List<List<Int>>) {
 }
 
 fun<T> List<T>.shift(int : Int = 1): List<T> {
-    val list = this.toMutableList()
+    val list = toMutableList()
     Collections.rotate(list, -int)
     return list
 }
